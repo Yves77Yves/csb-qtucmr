7c260ab7f01879060feb36c6c59706a09e49ad11
Code Sandbox <hello@codesandbox.io>
2022-08-09 14:18:29 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.3b6601ab3738cb9812a6e1886801abb2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c721a64d5520f34b8991f3fe7f5a5af",
+    "url": "https://yves77yves.github.io/csb-qtucmr/index.html"
+  },
+  {
+    "revision": "5dafd8c768f62f9ec5da",
+    "url": "https://yves77yves.github.io/csb-qtucmr/static/js/2.7d1b87d4.chunk.js"
+  },
+  {
+    "revision": "09484e5fa4db4817717f",
+    "url": "https://yves77yves.github.io/csb-qtucmr/static/js/main.94837e64.chunk.js"
+  },
+  {
+    "revision": "8b9853722395b4f14dc5",
+    "url": "https://yves77yves.github.io/csb-qtucmr/static/js/runtime-main.eb1b98d7.js"
+  }
+]);
